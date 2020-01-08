4bac5d30c9ad5a4a3b031e0a15915e8b191ca1a5
Eduardo Lowensohn <gunchosoft@gmail.com>
2020-01-08 00:02:45 -0300

    Assignment3

--- db/seed.rb
+++ db/seed.rb
@@ -12,4 +12,15 @@ puts 'Address Book created'
  puts Entry.create(address_book_id: book.id, name: 'Foo Two', phone_number: '111-111-1111', email: 'foo_two@gmail.com' )
  puts Entry.create(address_book_id: book.id, name: 'Foo Three', phone_number: '222-222-2222', email: 'foo_three@gmail.com' )
 
+ puts Entry.find_by('name', 'Foo One')
+
+ Entry.find_each(start: 1, batch_size: 10) do |row|
+        puts row
+
+ end
  
+ Entry.find_in_batches(start: 6, batch_size: 30) do |contacts|
+        contacts.each { |contact| puts contact }
+
+ end
+
