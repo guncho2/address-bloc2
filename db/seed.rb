32ff6fcdce48cd0a99975e088a3cb95d39c6b293
Eduardo Lowensohn <gunchosoft@gmail.com>
2020-01-28 21:25:43 -0300

    update2

--- db/seed.rb
+++ db/seed.rb
@@ -8,10 +8,13 @@ book = AddressBook.create(name: 'My Address Blook')
 
 puts 'Address Book created'
  puts 'Entry created'
-#  puts Entry.create(address_book_id: book.id, name: 'Foo One', phone_number: '999-999-9999', email: 'foo_one@gmail.com' )
-#  puts Entry.create(address_book_id: book.id, name: 'Foo Two', phone_number: '111-111-1111', email: 'foo_two@gmail.com' )
-#  puts Entry.create(address_book_id: book.id, name: 'Foo Three', phone_number: '222-222-2222', email: 'foo_three@gmail.com' )
-#  puts Entry.create(address_book_id: book.id, name: 'Eduardo', phone_number: '305-990-2218', email: 'eduardo@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'Foo One', phone_number: '999-999-9999', email: 'foo_one@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'Foo Two', phone_number: '111-111-1111', email: 'foo_two@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'Foo Three', phone_number: '222-222-2222', email: 'foo_three@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'Eduardo', phone_number: '305-990-2218', email: 'eduardo@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'juan', phone_number: '888-888-8888', email: 'juan@gmail.com' )
+ puts Entry.create(address_book_id: book.id, name: 'pedro', phone_number: '555-555-5555', email: 'pedro@gmail.com' )
+
 
 #  puts Entry.find_by('name', 'Foo One')
 
@@ -38,17 +41,29 @@ puts 'Address Book created'
 # puts Entry.order("name ASC, phone_number DESC")
 
 
- AddressBook.join(:entry)
+#  AddressBook.join(:entry)
 
-all_the_updates = { 1 => { "name" => "Dave", "email" => "david@bloc.com", "phone_number" => "111-111-1111" }, 2 => { "name" => "Jeremy", "email" => "jeremy@bloc.com", "phone_number" => "222-222-2222" }, 3 => { "name" => "Juan", "email" => "juan@bloc.com", "phone_number" => "333-333-3333" } }
-#  puts Entry.update_mult(all_the_updates.keys, all_the_updates.values)
- people =  Entry.update(all_the_updates.keys, all_the_updates.values)
-puts Entry.order("name ASC")
+# all_the_updates = { 1 => { "name" => "Dave", "email" => "david@bloc.com", "phone_number" => "111-111-1111" }, 2 => { "name" => "Jeremy", "email" => "jeremy@bloc.com", "phone_number" => "222-222-2222" }, 3 => { "name" => "Juan", "email" => "juan@bloc.com", "phone_number" => "333-333-3333" } }
+ #  puts Entry.update_mult(all_the_updates.keys, all_the_updates.values)
+#   people =  Entry.update(all_the_updates.keys, all_the_updates.values)
+# puts Entry.order("name ASC")
+
+
+Entry.destroy_all("name = 'Foo One'")
+Entry.destroy_all("phone_number = '305-990-2218'")
+Entry.destroy_all("email = 'foo_two@gmail.com'")
+Entry.destroy_all("phone_number = '222-222-2222'")
+
+
+
+
 
 
 #  people = Entry.all
-# puts Entry.all
+
+# Entry.not(name: "Foo Two")
  
+puts Entry.all
 
 #  people.update(name: "Dave", email: "david@bloc.com", phone_number: "111-111-1111", name: "Jaun", email: "juan@bloc.com", phone_number: "111-111-1111" )
 
@@ -74,14 +89,14 @@ puts Entry.order("name ASC")
 # puts Entry.all
 
 # people = Entry.all
-puts Entry.where(phone_number: '999-999-9999') 
+# puts Entry.where(phone_number: '999-999-9999')
 # where works
 
-puts people.not(phone_number: '999-999-9999')
+# puts people(phone_number: '999-999-9999').not
 # not is the only one not working with an error in a database : SQL /Users/eduardolowensohn/.rvm/gems/ruby-2.5.1/gems/sqlite3-1.4.1/lib/sqlite3/database.rb:147:in `initialize': near ";": syntax error (SQLite3::SQLException)
 
 # puts Entry.take(name: "Foo One")
-puts Entry.take(name: 'Dave')
+# puts Entry.take(name: 'Dave')
 
 # take fixed both take and take_one in  case  is string 
 
@@ -97,7 +112,8 @@ puts Entry.take(name: 'Dave')
 
 
 
-all_the_updates.not(name: "Dave")
+# all_the_updates(name: "Dave").not
+
 
 
 
@@ -108,11 +124,12 @@ all_the_updates.not(name: "Dave")
 
 
 
-Entry.destroy_all(["phone_number = ?", '111-111-1111'])
+Entry.destroy_all("phone_number = '999-999-9999'")
 
-Entry.where(name: "Juan").destroy_all
+# Entry.where(name: "Juan")
 
-puts Entry.all
+# puts Entry.all
+
 
 
 # all_the_updates.not(name: "Dave")
